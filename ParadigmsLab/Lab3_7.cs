792ab884e4a8f8b1d0740a065ce667b1d15b948d
unknown <dansru@gmail.com>
2019-04-20 18:25:47 +0300

    Meh

--- ParadigmsLab/Lab3_7.cs
+++ ParadigmsLab/Lab3_7.cs
@@ -19,6 +19,8 @@ namespace ParadigmsLab
         public List<List<List<int>>> allPossibleW;
         public string alphabet = "abcdefghijklmnopqrstuvwxyz";
         public string subAlphabet;
+        public List<int> wordd;
+        public List<List<int>> testRecursion;
 
         public Lab3_7(string w_word)
         {
@@ -62,12 +64,128 @@ namespace ParadigmsLab
                 else matrixOfTransitions[prevState, asciiChar] = nextState;
             }
             SetAllPossibleW();
+            SetPossibleWRecursive();
+            /*if(allPossibleW.Count > 1)
+            {
+                List<int> wordW = new List<int>();
+                int i = 0;
+                int j = 0;
+                int k = 0;
+                bool exit = false;
+                for(i = 0; i < allPossibleW.Count - 1; i++)
+                {
+                    exit = false;
+                    for(j = 0; j < allPossibleW.ElementAt(i).Count; j++)
+                    {
+                        for (k = 0; k < allPossibleW.ElementAt(i + 1).Count; k++)
+                        {
+                            if (allPossibleW.ElementAt(i).ElementAt(j).ElementAt(1) == allPossibleW.ElementAt(i + 1).ElementAt(k).ElementAt(0))
+                            {
+                                wordW.Add(allPossibleW.ElementAt(i).ElementAt(j).ElementAt(0));
+                                wordW.Add(allPossibleW.ElementAt(i).ElementAt(j).ElementAt(1));
+                                wordW.Add(allPossibleW.ElementAt(i + 1).ElementAt(k).ElementAt(0));
+                                wordW.Add(allPossibleW.ElementAt(i + 1).ElementAt(k).ElementAt(1));
+                                exit = true;
+                                break;
+                            }
+                        }
+                        if (!exit) break;
+                    }
+                }
+                this.wordd = wordW;
+            }*/
+
+            List<int> answer = ConnectStates(0, 3);
+        }
+
+
+        public void SetPossibleWRecursive()
+        {
+            testRecursion = new List<List<int>>();
+            foreach (List<int> l in allPossibleW.ElementAt(allPossibleW.Count - 1))
+            {
+                RecursiveWordW(l.ElementAt(0), l.ElementAt(1));
+            }
+        }
+
+        public void RecursiveWordW(int firstPairMember, int secondPairMember)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < amountOfStates && i != firstPairMember; i++)
+            {
+                for (int j = 0; j < amountOfChars; j++)
+                {
+                    if (matrixOfTransitions[i,j] == firstPairMember)
+                    {
+                        list.Add(i);
+                        list.Add(firstPairMember);
+                        this.wordd = list;
+                        RecursiveWordW(i, firstPairMember);
+
+                    }
+                }
+            }
+        }
+
+        public List<int> ConnectStates(int firstS, int secondS)
+        {
+            List<int> answer = new List<int>();
+
+            List<int> reachableFromFirst = new List<int>();
+            for(int i = 0; i < amountOfChars; i++)
+            {
+                if (matrixOfTransitions[firstS, i] == -2)
+                    continue;
+                else if (matrixOfTransitions[firstS, i] == -1)
+                    continue;
+                else reachableFromFirst.Add(matrixOfTransitions[firstS, i]);
+            }
+
+            List<int> priorForSecond = new List<int>();
+            for(int i = 0; i < amountOfStates; i++)
+            {
+                for(int j = 0; j < amountOfChars; j++)
+                {
+                    if (matrixOfTransitions[i, j] == secondS)
+                        priorForSecond.Add(i);
+
+                }
+            }
+
+            List<int> intersected = new List<int>();
+
+            for(int i = 0; i < priorForSecond.Count; i++)
+            {
+                for(int j = 0; j < reachableFromFirst.Count; j++)
+                {
+                    if(priorForSecond.ElementAt(i) == reachableFromFirst.ElementAt(j))
+                    {
+                        intersected.Add(priorForSecond.ElementAt(i));
+                    }
+                }
+            }
+
+            if(intersected.Count != 0)
+            {
+                answer.Add(firstS);
+                for(int i = 0; i < intersected.Count; i++)
+                {
+                    answer.Add(intersected.ElementAt(i));
+                }
+                answer.Add(secondS);
+
+            }
+
+
+
+            return null;
         }
 
         public void SetAllPossibleW()
         {
             this.allPossibleW = GetAllW(this.w);
         }
+
 
         public int[,] GetMatrixOfTransitions()
         {
@@ -98,6 +216,8 @@ namespace ParadigmsLab
             return allPairs;
         }
 
+
+
         public List<List<List<int>>> GetAllW(string word)
         {
             char[] wAsChar = word.ToCharArray();
@@ -111,10 +231,38 @@ namespace ParadigmsLab
             {
                 int letter = wAsInt[i];
 
-                List<List<int>> possiblePairsForLetter = GetAllPairsForLetter(0, amountOfStates, letter);
+                List<List<int>> possiblePairsForLetter = GetAllPairsForLetter(0, amountOfStates-1, letter);
                 listOfAllPairsForLetters.Add(possiblePairsForLetter);
             }
             return listOfAllPairsForLetters;
+        }
+
+        public void FindConnectionBetweenStates(int startState, int finState)
+        {
+            List<List<int>> possibleMiddleLetters = new List<List<int>>();
+            bool exit = false;
+            HashSet<int> firstStatesArr = new HashSet<int>();
+            firstStatesArr.Add(startState);
+            while (!exit)
+            {
+                for(int i = 0; i < firstStatesArr.Count; i++)
+                {
+                    if (exit) break;
+                    List<int> pair = new List<int>();
+                    if(matrixOfTransitions[startState,i] == finState)
+                    {
+                        exit = true;
+                        pair.Add(startState);
+                        pair.Add(finState);
+                        break;
+                    }
+                    else
+                    {
+                        //if(matrixOfTransitions[])
+                    }
+
+                }
+            }
         }
 
 
